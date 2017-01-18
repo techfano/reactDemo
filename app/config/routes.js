dddc36501f58cbe5331053c70c07e027771a097a
Estefano Castañeda <mixc21@gmail.com>
2017-01-18 17:11:56 -0500

    adding results and battle features

--- app/config/routes.js
+++ app/config/routes.js
@@ -7,6 +7,8 @@ var hashHistory = ReactRouter.hashHistory;
 var Main = require('../component/Main');
 var Home = require('../component/Home');
 var PromptContainer = require('../container/PromptContainer');
+var ConfirmBattleContainer =  require('../container/ConfirmBattleContainer');
+var ResultsContainer =  require('../container/ResultsContainer');
 
 var routes = (
   <Router history={hashHistory}>
@@ -14,6 +16,8 @@ var routes = (
       <IndexRoute component={Home}/>
       <Route path='playerOne' header='Player One' component={PromptContainer} />
       <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
+      <Route path='battle' component={ConfirmBattleContainer} />
+      <Route path='results' component={ResultsContainer} />
     </Route>
   </Router>
 );
